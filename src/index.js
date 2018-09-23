92157eedec053d4c621b4e9a3398efef62391160
petriken <petriken82@gmail.com>
2018-09-23 23:45:35 +0300

    sorter50/50

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+        // your implementation
   }
 
   add(element) {
+    this.arr.push(element);
     // your implementation
   }
 
   at(index) {
+    return this.arr[index];
     // your implementation
   }
 
   get length() {
+    return this.arr.length;
     // your implementation
   }
 
   toArray() {
+    return this.arr;
+    
     // your implementation
+  }
+    
+  sort(indices) {
+      var defaultcomp = function (a, b) {
+      return a-b;
+    };
+      var comp = defaultcomp;
+      if(this.comparator){
+      comp = this.comparator;
+    }
+    var tempArr = [];
+    for(var i = 0; i < indices.length; i ++)
+    {
+    tempArr.push(this.arr[indices[i]]);
+    }
+    
+    tempArr.sort(comp);
+    
+    for(var i = 0; i < indices.length; i ++)
+    {
+    this.arr[indices[i]] = tempArr[i];
+    }
+    
+    
+    
+         // your implementation
   }
 
-  sort(indices) {
-    // your implementation
-  }
 
   setComparator(compareFunction) {
+    this.comparator = compareFunction;
     // your implementation
   }
 }
